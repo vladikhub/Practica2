2e83d2ac34d947600a057f740b746c68439bcfac
smavl.andrey@gmail.com <smavl.andrey@gmail.com>
2024-02-14 23:41:59 +0700

    Закл версия

--- Market.java
+++ Market.java
@@ -46,35 +46,52 @@ public class Market {
    * номер параметра, который нужно сменить @param numAtr
    */
   public static void changeAttribute(Goods g, int numAtr){
-    switch (numAtr){
-      case 1:
-        System.out.println("Введите название категории: ");
-        String cat = sc.nextLine();
-        g.setCategory(cat);
-      case 2:
-        System.out.println("Введите название товара: ");
-        String name = sc.nextLine();
-        g.setName(name);
-      case 3:
-        System.out.println("Введите стоимость товара: ");
-        String costStr = sc.nextLine();
-        while (!costStr.matches("[0-9]\\d*")){
-          System.out.println("Введите правильно стоимомсть товара: ");
-          costStr = sc.nextLine();
-        }
-        int cost = Integer.parseInt(costStr);
-        g.setCost(cost);
-      case 4:
-        System.out.println("Введите вес товара (Ч.Ч): ");
-        String weightStr = sc.nextLine();
-        while (!weightStr.matches("(([0-9]\\d*).([0-9]\\d*))")){
-          System.out.println("Введите правильно вес товара: ");
-          weightStr = sc.nextLine();
-        }
-        double weight = Double.parseDouble(weightStr);
-        System.out.println("Введите вес товара (Ч.Ч): ");
-        g.setWeight(weight);
-    }
+
+      switch (numAtr){
+        case 1:
+          System.out.println("Введите название категории: ");
+          String cat = sc.nextLine();
+          g.setCategory(cat);
+          break;
+        case 2:
+          System.out.println("Введите название товара: ");
+          String name = sc.nextLine();
+          boolean exist = false;
+          for (Goods el: goods){
+            if (el.getName().equals(name)){
+              exist = true;
+            }
+          }
+          if (!exist){
+            g.setName(name);
+          } else{
+            System.out.println("Товар с таким названием уже существует");
+          }
+          break;
+        case 3:
+          System.out.println("Введите стоимость товара: ");
+          String costStr = sc.nextLine();
+          while (!costStr.matches("[0-9]\\d*")){
+            System.out.println("Введите правильно стоимомсть товара: ");
+            costStr = sc.nextLine();
+          }
+          int cost = Integer.parseInt(costStr);
+          g.setCost(cost);
+          break;
+        case 4:
+          System.out.println("Введите вес товара (Ч.Ч): ");
+          String weightStr = sc.nextLine();
+          while (!weightStr.matches("(([0-9]\\d*).([0-9]\\d*))")){
+            System.out.println("Введите правильно вес товара: ");
+            weightStr = sc.nextLine();
+          }
+          double weight = Double.parseDouble(weightStr);
+          System.out.println("Введите вес товара (Ч.Ч): ");
+          g.setWeight(weight);
+          break;
+      }
+
+
   }
 
   /**
@@ -91,10 +108,9 @@ public class Market {
   /**
    * Метод для добавления пустого объекта класса Goods
    */
-  public static void addPlaceForGood(){
+    public static void addPlaceForGood(){
     goods.add(new Goods());
   }
-
   /**
    * Метод для добавления товара (объекта класса Goods с веденными атрибутами)
    * Принимает название категории @param category,
@@ -103,7 +119,19 @@ public class Market {
    * вес товара @param weight
    */
   public static void addGood(String category, String name, int cost, double weight){
-    goods.add(new Goods(category, name, cost, weight));
+    boolean exist = false;
+    for (Goods g: goods){
+      if (g.getName().equals(name)){
+        exist = true;
+        break;
+      }
+    }
+    if (!exist){
+      goods.add(new Goods(category, name, cost, weight));
+    } else{
+      System.out.println("Товар с таким названием уже существует");
+    }
+
   }
 
   /**
@@ -118,10 +146,10 @@ public class Market {
         getAttrGood(g);
       } else{
         System.out.println("""
-              Категория: Не определено
-              Название: Не определено
-              Стоимость: Не определено
-              Вес: Не определено
+            Категория: Не определено
+            Название: Не определено
+            Стоимость: Не определено
+            Вес: Не определено
             """);
       }
     }
@@ -190,19 +218,80 @@ public class Market {
           }
           int num = Integer.parseInt(numStr);
           Goods g = goods.get(num-1);
-          System.out.println("""
+          if (g.getName().isEmpty()){
+            boolean runDestroy = false;
+            System.out.println("Введите категорию товара: ");
+            String cat = sc.nextLine();
+            System.out.println("Введите название товара: ");
+            String n = sc.nextLine();
+            while (true){
+              boolean exist = false;
+              for (Goods el: goods){
+                if (el.getName().equals(n)){
+                  exist = true;
+                  break;
+                }
+              }
+              if (!exist){
+                g.setName(n);
+                break;
+              } else{
+                System.out.println("Товар с таким названием уже существует");
+                System.out.println("""
+              Вы можете:
+              1. завершить этап замены атрибутов
+              2. продолжить""");
+                System.out.println("Выберите пункт: ");
+                String a = sc.nextLine();
+                while (!a.matches("[1-2]\\d*")){
+                  System.out.println("Введите правильно пункт: ");
+                  a = sc.nextLine();
+                }
+                if (a.equals("1")){
+                  runDestroy = true;
+                  break;
+                } else{
+                  System.out.println("Введите название товара: ");
+                  n = sc.nextLine();
+                }
+              }
+            }
+            if (runDestroy){
+              break;
+            }
+            System.out.println("Введите стоимость товара: ");
+            String cS = sc.nextLine();
+            while (!cS.matches("[0-9]\\d*")){
+              System.out.println("Введите правильно стоимомсть товара: ");
+              cS = sc.nextLine();
+            }
+            int c = Integer.parseInt(cS);
+            System.out.println("Введите вес товара (Ч.Ч): ");
+            String wS = sc.nextLine();
+            while (!wS.matches("(([0-9]\\d*).([0-9]\\d*))")){
+              System.out.println("Введите правильно вес товара: ");
+              wS = sc.nextLine();
+            }
+            double w = Double.parseDouble(wS);
+            g.setName(n);
+            g.setCost(c);
+            g.setCategory(cat);
+            g.setWeight(w);
+          } else{
+            System.out.println("""
             1. Категория
             2. Название
             3. Стоимость
             4. Вес
             """);
-          System.out.println("Введите номер атрибута, который будете менять: ");
-          String atr = sc.nextLine();
-          while (!atr.matches("[1-4]")){
-            System.out.println("Введите правильный номер атрибута: ");
-            atr = sc.nextLine();
-          }
-          changeAttribute(g, Integer.parseInt(atr));
+            System.out.println("Введите номер атрибута, который будете менять: ");
+            String atr = sc.nextLine();
+            while (!atr.matches("[1-4]")){
+              System.out.println("Введите правильный номер атрибута: ");
+              atr = sc.nextLine();
+            }
+            changeAttribute(g, Integer.parseInt(atr));
+        }
           break;
         case "5":
           sortGoodsByCost();
